aa7015b948f5f5ac23584ac76419e12f17b1d47c
thaihoc1994 <bboyfool215@gmail.com>
2018-07-06 17:16:37 +0700

    project structure

---
+++ ToeicOnline/toeic-web-logic/src/main/java/com/hochnt/web/logic/common/WebConstant.java
@@ -0,0 +1,8 @@
+package com.hochnt.web.logic.common;
+
+/**
+ * hochnt created
+ * contained constanst using in modules
+ */
+public class WebConstant {
+}
---
+++ ToeicOnline/toeic-web-logic/src/main/java/com/hochnt/web/logic/utils/Utils.java
@@ -0,0 +1,7 @@
+package com.hochnt.web.logic.utils;
+
+/**
+ * Contain common methods using in module
+ */
+public class Utils {
+}
